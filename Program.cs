af5897a46a10f5234e2d4c5aa9f6b91abf7333af
許建偉 <kenimai.tw@livemail.tw>
2023-01-30 10:10:15 +0800

    新增async

--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@
 using System.Linq;
 using System.IO;
 using System.Net;
+using System.Net.Http;
 using System.Text;
 using System.Text.Json;
 using System.Text.RegularExpressions;
@@ -9,7 +10,9 @@ using System.Web;
 using Newtonsoft.Json;
 using System.Diagnostics;
 using System.Runtime.InteropServices;
-
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
 
 namespace wnacg2
 {
@@ -19,7 +22,7 @@ namespace wnacg2
         public string caption { get; set; }
     }
 
-    class Program
+    public class Program
     {
         [DllImport("kernel32.dll", ExactSpelling = true)]
         private static extern IntPtr GetConsoleWindow();
@@ -32,7 +35,7 @@ namespace wnacg2
         private const int MINIMIZE = 6;
         private const int RESTORE = 9;
 
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
             //Console.SetWindowSize(Console.LargestWindowWidth, Console.LargestWindowHeight);
             ShowWindow(ThisConsole, MAXIMIZE);
@@ -44,9 +47,10 @@ namespace wnacg2
 
             string HtmlTitle = "";
             string HtmlBoby = "";
+
             string DownloadTitle = "";
 
-            //輸入作品編號
+            //輸入作品編號 
             Console.WriteLine("請輸入作品編號 : ");
             string DataNumber = Console.ReadLine().Trim();
 
@@ -54,7 +58,8 @@ namespace wnacg2
             string DataTitleUrl = "https://www.wnacg.org/photos-slide-aid-" + DataNumber + ".html";
 
             //作品圖片網頁
-            string DataImagesUrl = "https://www.wnacg.org/photos-gallery-aid-" + DataNumber + ".html";
+            //string DataImagesUrl = "https://www.wnacg.org/photos-gallery-aid-" + DataNumber + ".html";
+            string DataImagesUrl = "https://www.wnacg.org/photos-webp-aid-" + DataNumber + ".html";
 
             //取得作品標題
             using (WebClient DataTitleClient = new WebClient())
@@ -77,16 +82,38 @@ namespace wnacg2
 
                     if (MatchTitle.Success)
                     {
-                        //作品名稱
+                        //作品名稱  
                         DownloadTitle = MatchTitle.Groups[0].Value;
                         DownloadTitle = DownloadTitle.Replace("- 列表 - 紳士漫畫-專註分享漢化本子&#124;邪惡漫畫", "").Trim();
+
+                        //移除不必要標題內容
                         DownloadTitle = DownloadTitle.Replace("[中国翻訳]", "").Trim();
                         DownloadTitle = DownloadTitle.Replace("[中国翻译]", "").Trim();
                         DownloadTitle = DownloadTitle.Replace("[中國翻譯]", "").Trim();
 
+
                         //移除漢化組名稱
                         DownloadTitle = DownloadTitle.Replace("[羅莎莉亞漢化]", "").Trim();
                         DownloadTitle = DownloadTitle.Replace("[翻车汉化组]", "").Trim();
+                        DownloadTitle = DownloadTitle.Replace("[绅士仓库汉化]", "").Trim();
+                        DownloadTitle = DownloadTitle.Replace("[靴下汉化组]", "").Trim();
+                        DownloadTitle = DownloadTitle.Replace("[無邪気漢化組]", "").Trim();
+                        DownloadTitle = DownloadTitle.Replace("[甜族星人x我不看本子个人汉化]", "").Trim();
+                        DownloadTitle = DownloadTitle.Replace("[黑锅汉化组]", "").Trim();
+                        DownloadTitle = DownloadTitle.Replace("[夢之行蹤漢化組]", "").Trim();
+                        DownloadTitle = DownloadTitle.Replace("[白杨汉化组]", "").Trim();
+                        DownloadTitle = DownloadTitle.Replace("[黎欧出资汉化]", "").Trim();
+                        DownloadTitle = DownloadTitle.Replace("[不咕鸟汉化组]", "").Trim();
+                        DownloadTitle = DownloadTitle.Replace("[君日本語本當上手漢化組]", "").Trim();
+                        DownloadTitle = DownloadTitle.Replace("[空気系☆漢化]", "").Trim();
+                        DownloadTitle = DownloadTitle.Replace("[黑条汉化]", "").Trim();
+                        DownloadTitle = DownloadTitle.Replace("[CE家族社]", "").Trim();
+                        DownloadTitle = DownloadTitle.Replace("[无毒汉化组]", "").Trim();
+                        DownloadTitle = DownloadTitle.Replace("[紫苑汉化组]", "").Trim();
+                        DownloadTitle = DownloadTitle.Replace("[新桥月白日语社汉化]", "").Trim();
+                        DownloadTitle = DownloadTitle.Replace("[大鸟可不敢乱转汉化]", "").Trim();
+                        DownloadTitle = DownloadTitle.Replace("[裸單騎漢化]", "").Trim();
+                        DownloadTitle = DownloadTitle.Replace("[流木个人汉化]", "").Trim();
 
                         Console.WriteLine("作品名稱 : " + DownloadTitle);
                     }
@@ -122,6 +149,7 @@ namespace wnacg2
 
                     DataJson = DataJson.Replace("fast_img_host+\\\"", "");
                     DataJson = DataJson.Replace("\\\\\\\"", "");
+
                     DataJson = DataJson.Replace("//", "\"");
                     DataJson = DataJson.Replace(@"\""", "\"");
                     DataJson = DataJson.Replace("url: /themes/weitu/", "url: \"themes/weitu/");
@@ -158,55 +186,77 @@ namespace wnacg2
                             Directory.CreateDirectory(DirectoryPath);
                         }
 
-                        if (Directory.Exists(DirectoryPath))
+                        using (var client = new HttpClient())
                         {
-                            dynamic DI = JsonConvert.DeserializeObject(DataJson);
-
-                            int ImgCount = 1;
-
-                            foreach (var img in DI.DownloadImages)
+                            if (Directory.Exists(DirectoryPath))
                             {
-                                string ImgURL = img.url;
-                                string ImgCaption = img.caption;
+                                dynamic DI = JsonConvert.DeserializeObject(DataJson);
 
-                                //下載URL
-                                string DownloadImagesUrl = "https://" + ImgURL;
+                                int ImgTotalCount = 0;
+                                int ImgCount = 1;
 
-                                //取得主檔名
-                                string DownloadImagesName = ImgCaption.Trim();
-                                DownloadImagesName = DownloadImagesName.Replace("[", "");
-                                DownloadImagesName = DownloadImagesName.Replace("]", "");
-
-                                //取得副檔名
-                                string[] ImgSplit = ImgURL.Split('.');
-                                string DownloadImagesExt = ImgSplit[ImgURL.Split('.').Length - 1];
-
-                                //儲存檔案名稱
-                                string MainDownloadFile = DirectoryPath + DownloadImagesName + "." + DownloadImagesExt;
-
-                                if (!DownloadImagesUrl.Contains("themes"))
+                                if (((JArray)DI.DownloadImages).Count > 0)
                                 {
-                                    if (File.Exists(MainDownloadFile))
+                                    ImgTotalCount = ((JArray)DI.DownloadImages).Count;
+                                }
+
+                                foreach (var img in DI.DownloadImages)
+                                {
+                                    string ImgURL = img.url;
+                                    string ImgWebp = img.webp;
+                                    string ImgCaption = img.caption;
+                                    string DownloadImagesName = "";
+
+                                    //下載URL
+                                    string DownloadImagesUrl = "https://" + ImgURL;
+
+                                    //取得主檔名
+                                    //string DownloadImagesName = ImgCaption.Trim();
+                                    if (!string.IsNullOrEmpty(ImgWebp))
                                     {
-                                        Console.WriteLine("檔案已存在 : " + DownloadImagesUrl);
+                                        DownloadImagesName = ImgWebp.Trim();
+                                        DownloadImagesName = DownloadImagesName.Replace("[", "");
+                                        DownloadImagesName = DownloadImagesName.Replace("]", "");
                                     }
-                                    else
+
+                                    //取得副檔名
+                                    string[] ImgSplit = ImgURL.Split('.');
+                                    string DownloadImagesExt = ImgSplit[ImgURL.Split('.').Length - 1];
+
+                                    //儲存檔案名稱
+                                    string MainDownloadFile = DirectoryPath + DownloadImagesName + "." + DownloadImagesExt;
+
+                                    if (!DownloadImagesUrl.Contains("themes") && !string.IsNullOrEmpty(DownloadImagesName))
                                     {
-                                        using (WebClient DataClient = new WebClient())
+                                        if (File.Exists(MainDownloadFile))
                                         {
+                                            Console.WriteLine("檔案已存在 : " + DownloadImagesUrl);
+                                        }
+                                        else
+                                        {
+                                            Console.WriteLine("下載 : " + DownloadImagesUrl);
+                                            Console.WriteLine("下載時間 : " + DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss:fff"));
+
                                             try
                                             {
-                                                DataClient.Headers.Add("user-agent", "Mozilla/5.0 (Windows NT 6.1; Trident/7.0; rv:11.0) like Gecko");
+                                                using (var s = await client.GetStreamAsync(DownloadImagesUrl))
+                                                {
+                                                    using (var fs = new FileStream(MainDownloadFile, FileMode.CreateNew))
+                                                    {
+                                                        await s.CopyToAsync(fs);
 
-                                                DataClient.DownloadFile(DownloadImagesUrl, MainDownloadFile);
+                                                        //Console.WriteLine("完成 : " + DownloadImagesUrl + " KB : " + (fs.Length / 1000));
+                                                        Console.WriteLine("完成 : " + DownloadImagesUrl + " " + ImgCount + "/" + ImgTotalCount);
+                                                        Console.WriteLine("完成時間 : " + DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss:fff"));
+                                                        Console.WriteLine();
 
-                                                Console.WriteLine("下載 : " + DownloadImagesUrl + " [" + ImgCount + "]");
-
-                                                ImgCount += 1;
+                                                        ImgCount += 1;
+                                                    }
+                                                }
                                             }
-                                            catch
+                                            catch (Exception)
                                             {
-                                                Console.WriteLine("下載失敗 : " + DownloadImagesUrl + " [" + ImgCount + "]");
+                                                throw;
                                             }
                                         }
                                     }
@@ -216,9 +266,28 @@ namespace wnacg2
                     }
                 }
             }
+            else
+            {
+                Console.Write("No data...");
+            }
 
             Console.Write("Press any key to close the app...");
             Console.ReadKey();
+        }
+
+        public override bool Equals(object obj)
+        {
+            return base.Equals(obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+
+        public override string ToString()
+        {
+            return base.ToString();
         }
     }
 }
